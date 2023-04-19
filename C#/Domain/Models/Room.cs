e93d7bfa7c45ab434e721e7f3c9236f185be11ac
Maria-Elisabeta Mihai <mihai_elisabeta_3@yahoo.com>
2023-04-19 12:14:43 +0200

    add patient to room
    given a room id and a patient dto, i made a controller for patients with method that goes into patient logic where the patient is created and then uses the room dao to search for the room and add the created patient in the room's list

--- C#/Domain/Models/Room.cs
+++ C#/Domain/Models/Room.cs
@@ -9,7 +9,10 @@ public class Room
     public List<Patient> Patients { get; set; }
     public List <Sensor> Sensors { get; set; }
 
-    public Room() {}
+    public Room()
+    {
+        Patients = new List<Patient>();
+    }
 
     public Room(string name, int capacity, string availability, List<Sensor> sensors)
     {
