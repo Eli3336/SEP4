706bc9cfdaaf2c0f93c2cb2d041b348200898761
zahirkebab <93675640+zahirkebab@users.noreply.github.com>
2023-04-23 21:27:19 +0200

    Added frontend directory

---
+++ frontend/src/data/mockData.js
@@ -0,0 +1,125 @@
+const mockData = {
+  1: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  2: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+
+  3: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  4: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+
+  5: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  6: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+
+  7: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  8: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+
+  9: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  10: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+
+  11: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  12: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+
+  13: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  14: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+
+  15: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  16: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+  17: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  18: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+  19: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  20: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+  21: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+  22: {
+    temperature: 25,
+    humidity: 60,
+    patients: ["John Doe", "Jane Smith"],
+  },
+  23: {
+    temperature: 23,
+    humidity: 55,
+    patients: [],
+  },
+};
+export default mockData;
