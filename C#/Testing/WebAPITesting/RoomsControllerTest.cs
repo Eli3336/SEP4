e4694e42921f3518726076d9dbd6894beb270339
Maria-Elisabeta Mihai <mihai_elisabeta_3@yahoo.com>
2023-04-24 16:30:27 +0200

    Tests for new method getDetailsById + make all current tests for room pass

--- C#/Testing/WebAPITesting/RoomsControllerTest.cs
+++ C#/Testing/WebAPITesting/RoomsControllerTest.cs
@@ -50,8 +50,8 @@ public class RoomsControllerTest
             });
 
         // Assert
-        Assert.IsInstanceOf<CreatedAtActionResult>(result.Result);
-        var createdResult = (CreatedAtActionResult)result.Result;
+        Assert.IsInstanceOf<CreatedResult>(result.Result);
+        var createdResult = (CreatedResult)result.Result;
         Assert.AreSame(expectedRoom, createdResult.Value);
     }
 
@@ -71,6 +71,78 @@ public class RoomsControllerTest
         Assert.IsInstanceOf<OkObjectResult>(result.Result);
         var okResult = (OkObjectResult)result.Result;
         Assert.AreEqual(roomNames, okResult.Value);
+    }
+    
+    [Test]
+    public async Task GetAllRoomsName_ReturnsEmptyList()
+    {
+        // Arrange
+        var roomNames = new List<string> {};
+        roomLogicMock.Setup(mock => mock.GetAllNames())
+            .Returns(roomNames);
+
+        // Act
+        var result = await controller.GetAllRoomsName();
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result.Result);
+        var okResult = (OkObjectResult)result.Result;
+        Assert.AreEqual(roomNames, okResult.Value);
+    }
+    
+    
+    [Test]
+    public async Task GetRoomDetailsByIdAsync_ReturnsARoom()
+    {
+        // Arrange
+        var toSensor = new List<Sensor>
+        {
+            new Sensor { Id = 1,Type = "Temperature", UpBreakpoint = 2,DownBreakpoint = 2,Values = new List<SensorValue>()},
+            new Sensor { Id = 2,Type = "Humidity", UpBreakpoint = 2,DownBreakpoint = 2,Values = new List<SensorValue>() },
+            new Sensor { Id = 3,Type = "CO2", UpBreakpoint = 2,DownBreakpoint = 2,Values = new List<SensorValue>() }
+        };
+        var expectedRoom = new Room { 
+            Id = 23,
+            Name = "Test Room",
+            Capacity = 10,
+            Availability = "Available",
+            Sensors = toSensor
+        };
+        roomLogicMock.Setup(x => x.GetRoomDetailsByIdAsync(23)).ReturnsAsync(expectedRoom);
+
+        // Act
+        var result = await controller.GetRoomDetailsByIdAsync(23);
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result.Result);
+        var okResult = (OkObjectResult)result.Result;
+        Assert.AreEqual(expectedRoom, okResult.Value);
+    }
+    
+    [Test]
+    public async Task GetRoomDetailsByIdAsync_ReturnsNull_GivenNonexistentId()
+    {
+        // Arrange
+        var toSensor = new List<Sensor>
+        {
+            new Sensor { Id = 1,Type = "Temperature", UpBreakpoint = 2,DownBreakpoint = 2,Values = new List<SensorValue>()},
+            new Sensor { Id = 2,Type = "Humidity", UpBreakpoint = 2,DownBreakpoint = 2,Values = new List<SensorValue>() },
+            new Sensor { Id = 3,Type = "CO2", UpBreakpoint = 2,DownBreakpoint = 2,Values = new List<SensorValue>() }
+        };
+        var expectedRoom = new Room { 
+            Id = 23,
+            Name = "Test Room",
+            Capacity = 10,
+            Availability = "Available",
+            Sensors = toSensor
+        };
+        roomLogicMock.Setup(x => x.GetRoomDetailsByIdAsync(9999)).ReturnsAsync(expectedRoom);
+
+        // Act
+        var a = controller.GetRoomDetailsByIdAsync(9999);
+       
+        // Assert
+        Assert.IsNull(a.Result.Value);
     }
 }
 
