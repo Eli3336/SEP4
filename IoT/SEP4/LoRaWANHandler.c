2ff34b0d7dd85bb65a06e8ed4446b50d8ffaaa24
SeriousMonkeyU <berabbitshy@gmail.com>
2023-04-28 14:52:59 +0200

    Added CO2.c & CO2.h & Configuration.h & Configuration.c files. Required files: Temperature(.c/.h) and Humidity (.c/.h)

--- IoT/SEP4/LoRaWANHandler.c
+++ IoT/SEP4/LoRaWANHandler.c
@@ -13,8 +13,8 @@
 #include <status_leds.h>
 
 // Parameters for OTAA join - You have got these in a mail from IHA
-#define LORA_appEUI "XXXXXXXXXXXXXXX"
-#define LORA_appKEY "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
+#define LORA_appEUI "6BE1FDCE7E214CF9"
+#define LORA_appKEY "EECCD39BD2AB6C6BD107A08E0DBE9DB9"
 
 void lora_handler_task( void *pvParameters );
 
