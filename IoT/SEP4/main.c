1857b5320486f58e893c471a57ad62896667fe9a
Ancaelena2605 <112935366+Ancaelena2605@users.noreply.github.com>
2023-05-02 17:05:57 +0200

    changes x2

--- IoT/SEP4/main.c
+++ IoT/SEP4/main.c
@@ -29,6 +29,63 @@ SemaphoreHandle_t xTestSemaphore;
 // Prototype for LoRaWAN handler
 void lora_handler_initialise(UBaseType_t lora_handler_task_priority);
 
+//added sensors
+#include <queue.h>
+#include <event_groups.h>
+#include <mh_z19.h>
+#include <message_buffer.h>
+
+//#include <VibeHealth.h>
+#include <CO2Task.h>
+#include <Config.h>
+#include <HumiTempTask.h>
+
+// humidity + temperature
+static QueueHandle_t _humidityQueue;
+static QueueHandle_t _temperatureQueue;
+static QueueHandle_t _co2Queue;
+// servo
+// sender queues
+// breakpoint queues?
+// also receiver task
+
+static EventGroupHandle_t _actEventGroup;
+static EventGroupHandle_t _doneEventGroup;
+
+static SemaphoreHandle_t _mutex;
+
+static MessageBufferHandle_t _messageBuffer;
+
+static void _createQueues(void) {
+	_co2Queue = xQueueCreate(10, sizeof(uint16_t));
+	_humidityQueue = xQueueCreate(10, sizeof(uint16_t));
+	_temperatureQueue = xQueueCreate(10, sizeof(int16_t));
+	_messageBuffer = xMessageBufferCreate(sizeof(lora_driver_payload_t)*5);
+}
+
+static void _createEventGroups(void) {
+	_actEventGroup = xEventGroupCreate();
+	_doneEventGroup = xEventGroupCreate();
+}
+
+static void _createMutexes(void){
+	_mutex = xSemaphoreCreateMutex();
+}
+
+static void _initDrivers(void) {
+	puts("Initializing drivers...");
+	mh_z19_initialise(ser_USART3);
+	hih8120_initialise();
+	lora_driver_initialise(ser_USART1, _messageBuffer);
+}
+
+static void _createTasks(void) {
+	co2Task_create(_co2Queue, _actEventGroup, _doneEventGroup);
+	humiTempTask_create(_humidityQueue, _temperatureQueue, _actEventGroup, _doneEventGroup);
+}
+
+//added sensors end
+
 /*-----------------------------------------------------------*/
 void create_tasks_and_semaphores(void)
 {
@@ -100,7 +157,17 @@ int main(void)
 	initialiseSystem(); // Must be done as the very first thing!!
 	printf("Program Started!!\n");
 	vTaskStartScheduler(); // Initialise and run the freeRTOS scheduler. Execution should never return from here.
+stdio_initialise(ser_USART0);
 
+_createQueues();
+_initDrivers();
+_createEventGroups();
+_createTasks();
+_createMutexes();
+config_create(_mutex);
+
+puts("Starting...");
+vTaskStartScheduler();
 	/* Replace with your application code */
 	while (1)
 	{
