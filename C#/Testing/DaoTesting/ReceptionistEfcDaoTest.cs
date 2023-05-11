16741153fd65c1344f753844b8fc73011b872834
Maria-Elisabeta Mihai <mihai_elisabeta_3@yahoo.com>
2023-05-11 11:48:15 +0200

    created class, not done

---
+++ C#/Testing/DaoTesting/ReceptionistEfcDaoTest.cs
@@ -0,0 +1,18 @@
+﻿using EfcDataAccess;
+using Moq;
+using NUnit.Framework;
+
+namespace TestingReq5.DaoTesting;
+
+public class ReceptionistEfcDaoTest
+{
+    private Mock<> x;
+    private ReceptionistEfcDao dao;
+
+    [SetUp]
+    public void Setup()
+    {
+        x = new Mock<>();
+        dao = new ReceptionistEfcDao(x.Object);
+    }
+}
