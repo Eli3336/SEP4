dde7fd030dedfec2f5aaced57447bbce9a2ce84f
SeriousMonkeyU <berabbitshy@gmail.com>
2023-05-12 13:16:15 +0200

    Connection to BackEnd tested and successful.
    'main' was cleared
    'VibeController' was modified to be included
    Event Groups implemented for VibeController

--- IoT/SEP4/_c/CO2Task.c
+++ IoT/SEP4/_c/CO2Task.c
@@ -7,19 +7,19 @@
 #include <stdio.h>
 
 #define TASK_NAME "CO2Task"
-#define TASK_PRIORITY configMAX_PRIORITIES - 2
+#define TASK_PRIORITY configMAX_PRIORITIES - 3
 
 static void _co2CallBack(uint16_t ppm);
 static void _run(void* params);
 
 static QueueHandle_t _co2Queue;
-static EventGroupHandle_t _actEventGroup;
+static EventGroupHandle_t _doEventGroup;
 static EventGroupHandle_t _doneEventGroup;
 static uint16_t ppm;
 
-void co2Task_create(QueueHandle_t co2Queue, EventGroupHandle_t actEventGroup, EventGroupHandle_t doneEventGroup) {
+void co2Task_create(QueueHandle_t co2Queue, EventGroupHandle_t doEventGroup, EventGroupHandle_t doneEventGroup) {
 	_co2Queue = co2Queue;
-	_actEventGroup = actEventGroup;
+	_doEventGroup = doEventGroup;
 	_doneEventGroup = doneEventGroup;
 	
 	xTaskCreate(_run,
@@ -42,28 +42,25 @@ void co2Task_runTask() {
 	portMAX_DELAY
 	);
 	*/
-	puts("HI from CO2\n");
 	if ((mh_z19_takeMeassuring()) != MHZ19_OK) {
 		ppm = CONFIG_INVALID_CO2_VALUE;
 		_co2CallBack(ppm);
 	}
+		vTaskDelay(pdMS_TO_TICKS(30000UL));
 	
 	xEventGroupSetBits(_doneEventGroup, BIT_CO2_DONE);
 }
 
 static void _co2CallBack(uint16_t ppm){
-	printf("CO2 Value Is : %d \n",ppm);
 	
 	xQueueSendToBack(_co2Queue, &ppm, portMAX_DELAY);
-	
-	
 }
 
 static void _run(void* params) {
+	
 	co2Task_initTask(params);
-	co2Task_runTask();
 	
 	while (1) {
-		
+		co2Task_runTask();
 	}
 }
