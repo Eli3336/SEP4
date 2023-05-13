ad29d73546723b8b554d39355e9763792333efb7
Maria-Elisabeta Mihai <mihai_elisabeta_3@yahoo.com>
2023-05-13 16:53:41 +0200

    enpoints to get request by id, delete by id and get requests

---
+++ C#/Application/LogicInterfaces/IRequestLogic.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Application.LogicInterfaces;
+
+public interface IRequestLogic
+{
+    Task<Request?> GetByIdAsync(int id);
+    Task DeleteAsync(int id);
+    Task<IEnumerable<Request>> GetAllRequestsToMovePatients();
+    Task<IEnumerable<Request>> GetAllAdditionalRequests();
+}
---
+++ C#/EfcDataAccess/RequestEfcDao.cs
@@ -0,0 +1,40 @@
+﻿using Application.DaoInterfaces;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfcDataAccess;
+
+public class RequestEfcDao: IRequestDao
+{
+    private readonly HospitalContext context;
+
+    public RequestEfcDao(HospitalContext context)
+    {
+        this.context = context;
+    }
+    
+    public async Task<Request?> GetByIdAsync(int id)
+    {
+        Request? found = await context.Request
+            .SingleOrDefaultAsync(doctor => doctor.Id == id);
+        return found;
+    }
+    
+    public async Task DeleteAsync(int id)
+    {
+        Request? existing = await GetByIdAsync(id);
+        if (existing == null)
+        {
+            throw new Exception($"Request with id {id} not found");
+        }
+        context.Request.Remove(existing);
+        await context.SaveChangesAsync();  
+    }
+    
+    public async Task<IEnumerable<Request>> GetAllRequests()
+    {
+        IQueryable<Request> receptionists = context.Request.AsQueryable();
+        IEnumerable<Request> result = await receptionists.ToListAsync();
+        return result;
+    }
+}
