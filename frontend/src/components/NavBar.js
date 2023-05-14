0640fdb632421136537023089542fd24382f5539
marwa93nezar <304221@via.dk>
2023-05-14 15:29:22 +0200

    Doctor Page updated

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -21,6 +21,11 @@ const NavBar = () => {
             <a>Admin</a>
           </Link>
         </li>
+        <li>
+          <Link href="/doctor">
+            <a>Doctor</a>
+          </Link>
+        </li>
       </ul>
       <style jsx>{`
         nav {
