d4dabd1a8417cc61bde6fcb7c9f1c25f26c36df4
domignat <92146634+domignat@users.noreply.github.com>
2023-05-14 17:13:11 +0200

    Test get all additional requests

---
+++ C#/Testing/ApplicationTesting/RequestLogicTest.cs
@@ -0,0 +1,32 @@
+﻿using Application.DaoInterfaces;
+using Application.Logic;
+using Domain.DTOs;
+using Domain.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace TestingReq5.ApplicationTesting;
+
+public class RequestLogicTest
+{
+    [Test]
+    public async Task GetAllAdditionalRequests_ReturnsRequestsToMove()
+    {
+        var requestDaoMock = new Mock<IRequestDao>();
+        var expectedRequests = new List<Request>
+        {
+            new Request { Type = "Additional", Content = "Make sure room 2 has a temp lower than 20"},
+            new Request { Type = "Additional", Content = "Make sure room 1 has a temp higher than 20"},
+        };
+        requestDaoMock.Setup(dao => dao.GetAllRequests())
+            .ReturnsAsync(expectedRequests);
+
+        var requestService = new RequestLogic(requestDaoMock.Object);
+
+        var requestValues = await requestService.GetAllAdditionalRequests();
+
+        Assert.IsNotNull(requestValues);
+        Assert.AreEqual(expectedRequests.Count, requestValues.Count());
+        
+    }
+}
