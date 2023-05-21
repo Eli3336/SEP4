ca73af35d13e49a33efb9f1e05d4c5e8a710faed
BiancaBadeu <92147004+BiancaBadeu@users.noreply.github.com>
2023-05-22 00:05:57 +0200

    controller

---
+++ C#/Domain/DTOs/SensorUpdateDto.cs
@@ -0,0 +1,19 @@
+namespace Domain.DTOs;
+
+public class RoomUpdateDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int? Capacity { get; set; }
+    public string? Availability { get; set; }
+    
+    public RoomUpdateDto(){}
+
+    public RoomUpdateDto(int id, string? name, int? capacity, string? availability)
+    {
+        Id = id;
+        Name = name;
+        Capacity = capacity;
+        Availability = availability;
+    }
+}
