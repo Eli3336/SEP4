35c0bb92d3da03b701f57d97c81427625fa8ed41
BiancaBadeu <92147004+BiancaBadeu@users.noreply.github.com>
2023-05-22 00:32:15 +0200

    logic updSens

--- C#/Application/Logic/SensorLogic.cs
+++ C#/Application/Logic/SensorLogic.cs
@@ -36,5 +36,45 @@ public class SensorLogic : ISensorLogic
     {
         return await sensorDao.GetLogOfSensorValuesAsync(sensorId);
     }
+    
+    public async Task SensorUpdateAsync(int id, double upbreakpoint, double downbreakpoint)
+    {
+        Sensor? existing = await sensorDao.GetByIdToUpdateAsync(id);
+        if (existing == null)
+        {
+            throw new Exception($"Sensor with ID {id} not found!");
+        }
+        
+       SensorUpdateDto dto = new SensorUpdateDto(id, upbreakpoint, downbreakpoint);
+
+        double? upToUse = dto.UpBreakPoint ?? existing.UpBreakpoint;
+        double? downToUse = dto.DownBreakPoint ?? existing.DownBreakpoint;
+        
+        
+        Sensor updated = new (upToUse, downToUse)
+        {
+            Id = existing.Id,
+            Type = existing.Type,
+            Values = existing.Values
+        };
+        ValidateSensorUpdate(updated);
+        await sensorDao.SensorUpdateAsync(updated);
+    }
+    
+    private void ValidateSensorUpdate(Sensor sensor)
+    {
+        if (sensor.Type.Equals("Temperature") && sensor.DownBreakpoint<-50 && sensor.UpBreakpoint > 50)
+        {
+            throw new ArgumentException("The Temperature is invalid!");
+        }
+        if (sensor.Type.Equals("Humidity") && sensor.DownBreakpoint<10 && sensor.UpBreakpoint > 80)
+        {
+            throw new ArgumentException("The humidity is invalid!");
+        }
+        if (sensor.Type.Equals("CO2") && sensor.DownBreakpoint< && sensor.UpBreakpoint > 80)
+        {
+            throw new ArgumentException("The room can only be Available or Under maintenance. Please choose one or check for typos!");
+        }
+    }
 
 }
