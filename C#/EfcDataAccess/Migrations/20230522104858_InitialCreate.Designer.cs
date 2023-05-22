a31b7b8654205106aa5df1229c8d43eb00a853e8
Maria-Elisabeta Mihai <mihai_elisabeta_3@yahoo.com>
2023-05-22 12:50:16 +0200

    database fix to show admin table on my computer

--- C#/EfcDataAccess/Migrations/20230522104858_InitialCreate.Designer.cs
+++ C#/EfcDataAccess/Migrations/20230522104858_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EfcDataAccess.Migrations
 {
     [DbContext(typeof(HospitalContext))]
-    [Migration("20230521231106_InitialCreate")]
+    [Migration("20230522104858_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
