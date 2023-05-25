c4b4c300888818d8c84dbdcca36b315af065ebce
Viks Doe <viktor.vegh7@gmail.com>
2023-05-25 18:30:49 +0200

    bugs fixed plus requirement 14 done

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -10,7 +10,7 @@ const HomePage = () => {
         Please select a role to continue: <Link href="/user">User</Link>,{" "}
         <Link href="/assign-room">Receptionist</Link>,{" "}
         <Link href="/room-management">Admin</Link> or{" "}
-        <Link href="/DoctorManagementPage">Doctor</Link>
+        <Link href="/doctor">Doctor</Link>
       </p>
     </div>
   );
